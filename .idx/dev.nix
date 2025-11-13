fe6575a78e6278a191e750639077281c558ca6a6
R2Ksanu <sebsnm2012@gmail.com>
2025-11-13 20:01:26 +0530

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,12 +1,13 @@
-# dev.nix
-with import <nixpkgs> {};
+{ pkgs, ... }: {
+  channel = "stable-24.05";
 
-mkShell {
-  # Packages to install in the shell
-  buildInputs = [
+  packages = with pkgs; [
     unzip
     openssh
     git
+    bash
+    curl
+    apt
     qemu_kvm
     sudo
     cdrkit
@@ -14,15 +15,11 @@ mkShell {
     qemu
   ];
 
-  # Environment variables
-  shellHook = ''
-    export EDITOR=nano
-  '';
+  env = {
+    EDITOR = "nano";
+  };
 
-  # Optional: Workspace / IDE extensions placeholders
-  # These are just informational; nix-shell won't use them automatically
   idx = {
-    channel = "stable-24.05";
     extensions = [
       "Dart-Code.flutter"
       "Dart-Code.dart-code"
